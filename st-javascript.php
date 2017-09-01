baa38476ec455b7726093458e4435b94f1e30bc2
Punit Panchal <punitpanchal86@gmail.com>
2017-09-01 14:51:38 +0530

    Removed Unwanted files

--- st-javascript.php
+++ st-javascript.php
@@ -52,7 +52,6 @@
 		<script src="assets/js/jquery-1.10.2.min.js"></script>
 		<script src="assets/js/jquery-migrate-1.2.1.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="http://maps.google.com/maps/api/js?key=AIzaSyDDZJO4F0d17RnFoi1F2qtw4wn6Wcaqxao&amp;sensor=false&amp;language=en"></script>		<script src="assets/js/gmap3.min.js"></script>
 		<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
 		<script src="assets/js/owl.carousel.min.js"></script>
 		<script src="assets/js/css_browser_selector.min.js"></script>
