f7397dcd8a7a8e55640c30ba2d244f0400e448fb
Satish <satishdhere007@gmail.com>
2017-09-09 13:53:59 +0530

    license table added

--- page-profile.php
+++ page-profile.php
@@ -1926,7 +1926,7 @@ else
 					type: "POST",
 					data: new FormData(this), // Data sent to server, a set of key/value pairs (i.e. form fields and values)
 					contentType: false,       // The content type used when sending data to the server.
-					cache: false,             // To unable request pages to be cached
+					cache: false,             // To unabmale request pages to be cached
 					processData:false,        // To send DOMDocument or non processed data file it is set to false
 					async:true,						
 					success: function(response) 
